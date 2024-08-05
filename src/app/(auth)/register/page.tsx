9f463e3d39b0d830e154af927c03ad3c3b826ff4
Kurtmiguel <149345822+Kurtmiguel@users.noreply.github.com>
2024-08-05 16:40:43 +0800

    3rd

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,22 +1,12 @@
+import { RegisterForm } from '@/components/RegistrationForm'
 import React from 'react'
-import { useForm, FormProvider } from 'react-hook-form'
-import { RegisterForm} from '@/components/RegistrationForm'
-import { Header } from '@/components/Header'
 
-const UserRegister: React.FC = () => {
-  const methods = useForm()
-
+const Register = () => {
   return (
-    <div>
-      <Header />
-      <main className="p-4">
-        <h2 className="text-xl font-bold mb-4">User Registration</h2>
-        <FormProvider {...methods}>
-          <RegisterForm />
-        </FormProvider>
-      </main>
-    </div>
+    <>
+      <RegisterForm />
+    </>
   )
 }
 
-export default UserRegister
+export default Register
