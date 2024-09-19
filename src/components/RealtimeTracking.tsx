437ec16feb430384f6bb8c713da4b99764b6cd24
Kurtmiguel <149345822+Kurtmiguel@users.noreply.github.com>
2024-09-19 16:43:48 +0800

    Admin-dashboard Posts Page.

--- src/components/RealtimeTracking.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function RealTimeTracking() {
-  return (
-    <div className="h-full flex items-center justify-center">
-      <p>Real-time tracking data will be displayed here.</p>
-    </div>
-  );
-}
